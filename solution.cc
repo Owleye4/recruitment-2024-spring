61a631dac4cec849597b05db22197df9e55fd392
oy4 <owleye4202@gmail.com>
2024-06-06 12:16:55 +0800

    optimized phase1 with ttb::parallel_sort

--- solution.cc
+++ solution.cc
@@ -2,7 +2,6 @@
 #include <cstddef>
 #include <cstdlib>
 #include <omp.h>
-#include <vector>
 #include <tbb/parallel_sort.h>
 
 void optimized_pre_phase1(size_t) {}
@@ -14,12 +13,7 @@ void optimized_pre_phase2(size_t) {}
 void optimized_post_phase2() {}
 
 void optimized_do_phase1(float* data, size_t size) {
-    std::vector<float> float_data(data, data + size);
-    tbb::parallel_sort(float_data.begin(), float_data.end());
-#   pragma omp parallel for num_threads(128)
-    for (size_t i = 0; i < size; ++i) {
-        data[i] = float_data[i];
-    }
+    tbb::parallel_sort(data, data + size)
 }
 
 void optimized_do_phase2(size_t* result, float* data, float* query, size_t size) {
